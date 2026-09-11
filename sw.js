ea5d1d8ddf1682de96a943da0aa309875ebb02b8
zhounengshan <zhounengshan@gmail.com>
2026-09-11 13:07:38 +0800

    Refresh service worker cache for touch update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='casual-tetris-v1';
+const CACHE='casual-tetris-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
