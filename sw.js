9ddfa83c56eed49601d426fb463cc13d7bd11177
zhounengshan <zhounengshan@gmail.com>
2026-09-13 18:56:10 +0800

    Refresh cache for mobile Tetris update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='casual-tetris-v2';
+const CACHE='casual-tetris-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
